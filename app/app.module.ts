6abc862376a9bd3af706b661c0e832fc2f1544d8
Bill Kilmer <wdk122@gmail.com>
2016-09-26 14:44:52 -0700

    HTTP

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,32 +1,41 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import './rxjs-extensions';
 
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }  from './dashboard.component';
-import { HeroService }         from './hero.service';
-import { routing }             from './app.routing';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
+
+// Imports for loading and configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroSearchComponent }  from './hero-search.component';
+import { HeroesComponent }      from './heroes.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroService }          from './hero.service';
+import { routing }              from './app.routing';
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [ 
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
+    HeroSearchComponent,
     HeroesComponent
   ],
   providers: [
     HeroService
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [ AppComponent ]
 })
-
-export class AppModule { }
+export class AppModule {
+}
